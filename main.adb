d3090be651d21bcd4b432609a9668bc80071bfd2
Johannes Kliemann <kliemann@componolit.com>
2020-03-24 16:35:54 +0100

    change LED state on button press

--- main.adb
+++ main.adb
@@ -6,26 +6,19 @@ procedure Main
 is
    package GPIO renames Componolit.Runtime.Drivers.GPIO;
    package RCC renames Componolit.Runtime.Drivers.RCC;
-   procedure Wait;
-   procedure Wait
-   is
-   begin
-      for I in Integer range 0 .. 100000 loop
-         null;
-      end loop;
-   end Wait;
    LD3 : constant GPIO.Pin := GPIO.PC8;
    LD4 : constant GPIO.Pin := GPIO.PC9;
+   RD  : constant GPIO.Pin := GPIO.PC0;
+   RDV : GPIO.Value;
 begin
    RCC.Set (RCC.IOPC, True);
    GPIO.Configure (LD3, GPIO.Port_Out);
    GPIO.Configure (LD4, GPIO.Port_Out);
+   GPIO.Configure (RD, GPIO.Port_In, GPIO.Low);
+   GPIO.Write (LD3, GPIO.High);
+   GPIO.Write (LD4, GPIO.Low);
    loop
-      GPIO.Write (LD3, GPIO.High);
-      Wait;
-      GPIO.Write (LD4, GPIO.High);
-      GPIO.Write (LD3, GPIO.Low);
-      Wait;
-      GPIO.Write (LD4, GPIO.Low);
+      GPIO.Read (RD, RDV);
+      GPIO.Write (LD4, RDV);
    end loop;
 end Main;
