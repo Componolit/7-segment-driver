ba4b864c96c1b3aa174a8728e5603665b1926826
Johannes Kliemann <kliemann@componolit.com>
2020-03-25 16:40:48 +0100

    7 segment display schema

--- src/main.adb
+++ src/main.adb
@@ -10,13 +10,29 @@ is
    LD4 : constant GPIO.Pin := GPIO.PC9;
    RD  : constant GPIO.Pin := GPIO.PC0;
    RDV : GPIO.Value;
+   procedure Enable_Segment (Pin : GPIO.Pin);
+   procedure Enable_Segment (Pin : GPIO.Pin)
+   is
+   begin
+      GPIO.Configure (Pin, GPIO.Port_Out);
+      GPIO.Write (Pin, GPIO.High);
+   end Enable_Segment;
 begin
    RCC.Set (RCC.IOPC, True);
+   RCC.Set (RCC.IOPB, True);
    GPIO.Configure (LD3, GPIO.Port_Out);
    GPIO.Configure (LD4, GPIO.Port_Out);
    GPIO.Configure (RD, GPIO.Port_In, GPIO.Low);
    GPIO.Write (LD3, GPIO.High);
    GPIO.Write (LD4, GPIO.Low);
+   Enable_Segment (GPIO.PB4);
+   Enable_Segment (GPIO.PC5);
+   Enable_Segment (GPIO.PB0);
+   Enable_Segment (GPIO.PB1);
+   Enable_Segment (GPIO.PB2);
+   Enable_Segment (GPIO.PB10);
+   Enable_Segment (GPIO.PB11);
+   Enable_Segment (GPIO.PB12);
    loop
       GPIO.Read (RD, RDV);
       GPIO.Write (LD4, RDV);
