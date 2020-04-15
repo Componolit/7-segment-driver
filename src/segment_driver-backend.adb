a21365d7840260585c28c9f882cfeab1337c54a6
Isabell Zorr <zorr@componolit.com>
2020-04-15 12:26:10 +0200

    adapt to Componolit Style guide

--- src/segment_driver-backend.adb
+++ src/segment_driver-backend.adb
@@ -39,42 +39,42 @@ package body Segment_Driver.Backend is
 
       type Value_Array is array (Index'Range) of GPIO.Value;
 
-      values : Value_Array;
+      Values : Value_Array;
    begin
-      values (1) := (if Value in 0 | 2 | 3 | 5 | 6 | 7 | 8 | 9 | 10 | 12 | 14
+      Values (1) := (if Value in 0 | 2 | 3 | 5 | 6 | 7 | 8 | 9 | 10 | 12 | 14
                       | 15
                     then
                        GPIO.High
                     else
                        GPIO.Low); --  A
-      values (2) := (if Value in 0 | 1 | 2 | 3 | 4 | 7 | 8 | 9 | 10 | 13
+      Values (2) := (if Value in 0 | 1 | 2 | 3 | 4 | 7 | 8 | 9 | 10 | 13
                     then
                        GPIO.High
                     else
                        GPIO.Low); --  B
-      values (3) := (if Value in 0 | 1 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
+      Values (3) := (if Value in 0 | 1 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
                        | 13
                       then
                        GPIO.High
                     else
                        GPIO.Low); --  C
-      values (4) := (if Value in 0 | 2 | 3 | 5 | 6 | 8 | 9 | 11 | 12 | 13 | 14
+      Values (4) := (if Value in 0 | 2 | 3 | 5 | 6 | 8 | 9 | 11 | 12 | 13 | 14
                     then
                        GPIO.High
                     else
                        GPIO.Low); --  D
-      values (5) := (if Value in 0 | 2 | 6 | 8 | 10 | 11 | 12 | 13 | 14 | 15
+      Values (5) := (if Value in 0 | 2 | 6 | 8 | 10 | 11 | 12 | 13 | 14 | 15
                     then
                        GPIO.High
                     else
                        GPIO.Low); --  E
-      values (6) := (if Value in 0 | 4 | 5 | 6 | 8 | 9 | 10 | 11 | 12 | 14
+      Values (6) := (if Value in 0 | 4 | 5 | 6 | 8 | 9 | 10 | 11 | 12 | 14
                       | 15
                     then
                        GPIO.High
                     else
                        GPIO.Low); -- F
-      values (7) := (if Value in 2 | 3 | 4 | 5 | 6 | 8 | 9 | 10 | 11 | 13
+      Values (7) := (if Value in 2 | 3 | 4 | 5 | 6 | 8 | 9 | 10 | 11 | 13
                       | 14 | 15
                     then
                        GPIO.High
@@ -82,7 +82,7 @@ package body Segment_Driver.Backend is
                        GPIO.Low); -- G
 
       for I in Index'Range loop
-         GPIO.Write (Pins (I), values (I));
+         GPIO.Write (Pins (I), Values (I));
       end loop;
    end Show;
 
--- src/segment_driver-backend.ads
+++ src/segment_driver-backend.ads
@@ -15,7 +15,7 @@ package Segment_Driver.Backend is
 private
    type Index_Circle is range 1 .. 6;
    type Segment_Array_Circle is array (Index_Circle'Range) of GPIO.Pin;
-   Arr : constant Segment_Array_Circle := (A, B, C, D, E, F);
+   Arr  : constant Segment_Array_Circle := (A, B, C, D, E, F);
    type Index is range 1 .. 7;
    type Segment_Array is array (Index'Range) of GPIO.Pin;
    Pins : constant Segment_Array := (A, B, C, D, E, F, G);
