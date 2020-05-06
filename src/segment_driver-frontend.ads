418d31438a9bf914fd8c8d08dec7a25da6ad8a83
Isabell Zorr <zorr@componolit.com>
2020-05-06 16:38:35 +0200

    Prove pin configuration
    ref #5

--- src/segment_driver-frontend.ads
+++ src/segment_driver-frontend.ads
@@ -6,9 +6,27 @@ generic
 package Segment_Driver.Frontend with
    SPARK_Mode
 is
+   pragma Unevaluated_Use_Of_Old (allow);
+   use type GPIO.Mode;
 
-   procedure Read (Value : out Nibble);
+   procedure Read (Value : out Nibble) with
+     Pre  => (for all P in GPIO.Pin => GPIO.Valid (P))
+             and then GPIO.Pin_Mode (Bit_0) = GPIO.Port_In
+             and then GPIO.Pin_Mode (Bit_1) = GPIO.Port_In
+             and then GPIO.Pin_Mode (Bit_2) = GPIO.Port_In
+             and then GPIO.Pin_Mode (Bit_3) = GPIO.Port_In,
+     Post => (for all P in GPIO.Pin => GPIO.Valid (P))
+             and then (for all P in GPIO.Pin => GPIO.Pin_Modes (P) = GPIO.Pin_Modes'Old (P));
 
-   procedure Initialize;
+   procedure Initialize with
+     Pre  => (for all P in GPIO.Pin => GPIO.Valid (P)),
+     Post => (for all P in GPIO.Pin => GPIO.Valid (P))
+             and then GPIO.Pin_Mode (Bit_0) = GPIO.Port_In
+             and then GPIO.Pin_Mode (Bit_1) = GPIO.Port_In
+             and then GPIO.Pin_Mode (Bit_2) = GPIO.Port_In
+             and then GPIO.Pin_Mode (Bit_3) = GPIO.Port_In
+             and then (for all P in GPIO.Pin =>
+                         (if P not in Bit_0 | Bit_1 | Bit_2 | Bit_3
+                              then GPIO.Pin_Modes (P) = GPIO.Pin_Modes'Old (P)));
 
 end Segment_Driver.Frontend;
